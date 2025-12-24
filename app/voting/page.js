46f14d03b87baa3cd96ea172583cd2769f1f9642
possiblytrashy <oustedad@gmail.com>
2025-12-24 15:14:07 +0000

    Update page.js

--- app/voting/page.js
+++ app/voting/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 import { 
   Zap, Trophy, Search, TrendingUp, ArrowLeft, 
   ChevronRight, Share2, XCircle, BarChart3, Send 
