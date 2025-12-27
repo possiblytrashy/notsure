e89376fd031ffe72cc226daa28814bc3ff78744e
possiblytrashy <oustedad@gmail.com>
2025-12-27 20:23:58 +0000

    Update page.js

--- app/dashboard/organizer/page.js
+++ app/dashboard/organizer/page.js
@@ -259,7 +259,7 @@ export default function OrganizerDashboard() {
   const saveCompEdit = async () => {
     if (!showEditCompModal?.id) return;
     setIsProcessing(true);
-    try {
+   try {
       const { error } = await supabase
         .from('contests')
         .update({ 
@@ -270,7 +270,7 @@ export default function OrganizerDashboard() {
           is_active: editCompForm.is_active,
           image_url: editCompForm.image_url
         })
-        .eq('id', showEditCompModal.id);
+        .eq('id', showEditCompModal.id)
 
       if (error) throw error;
       
