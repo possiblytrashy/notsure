197eab515b753da539b844ad16d8ad0a095eb4b7
possiblytrashy <oustedad@gmail.com>
2026-01-11 01:18:31 +0000

    Update route.js

--- app/api/paystack/initialize/route.js
+++ app/api/paystack/initialize/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase clients
-const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY; // server-side key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
