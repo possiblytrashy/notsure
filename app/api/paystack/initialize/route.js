efdda0c846cc4523d1e78bbe2635d3c2b38017e9
possiblytrashy <oustedad@gmail.com>
2026-01-11 01:23:47 +0000

    Update route.js

--- app/api/paystack/initialize/route.js
+++ app/api/paystack/initialize/route.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // --- Initialize Supabase clients ---
 const supabaseAdmin = createClient(
-  process.env.SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY // Server-side key
 );
 
