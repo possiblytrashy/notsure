53725423c4e0ae018039947b73c2ab01452e90bd
possiblytrashy <oustedad@gmail.com>
2026-03-14 18:34:25 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,10 +13,10 @@ const nextConfig = {
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { 
-  key: 'Permissions-Policy', 
-  value: 'camera=*, microphone=*, geolocation=*' 
-},
+          {
+  key: 'Permissions-Policy',
+  value: 'camera=(self), microphone=(self), geolocation=(self)'
+}
           {
             key: 'Content-Security-Policy',
             value: [
